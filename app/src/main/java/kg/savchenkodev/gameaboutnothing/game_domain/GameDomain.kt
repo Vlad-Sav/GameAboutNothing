1c85e2e0646e1d590b14800739b110bed1da62b9
Vladlen Savchenko <vladlen.savchenko@myworkflow.ru>
2024-11-01 16:51:21 +0600

    init level

--- app/src/main/java/kg/savchenkodev/gameaboutnothing/game_domain/GameDomain.kt
+++ app/src/main/java/kg/savchenkodev/gameaboutnothing/game_domain/GameDomain.kt
@@ -1,8 +1,21 @@
 package kg.savchenkodev.gameaboutnothing.game_domain
 
+import kg.savchenkodev.gameaboutnothing.model.GameObject
+
 class GameDomain {
+    private var currentLevel: Level? = null
 
-    fun loadGame() {
-
+    suspend fun loadLevel() {
+        currentLevel = Level(
+            character = GameObject.Character(
+                coordinates = Coordinates(x = 2, y = 3)
+            ),
+            coin = GameObject.Coin(
+                coordinates = Coordinates(x = 2, y = 0)
+            ),
+            field = GameObject.Field(
+                size = Size(4, 4)
+            )
+        )
     }
 }
